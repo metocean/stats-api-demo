1e1ac198c09508abca1f408028bc781a35c7b6a9
davemetocean <dave@ngaru.rag.metocean.co.nz>
2017-06-22 18:12:19 +1200

    Working demo

---
+++ _site/js/main.js
@@ -0,0 +1,11 @@
+var Client = require('node-rest-client').Client;
+var d3 = require('d3');
+var options = {
+    mimetypes: {
+        json: ["application/json", "application/vnd.metocean.cf+json"]
+    }
+};
+var client=new Client(options);
+
+
+
